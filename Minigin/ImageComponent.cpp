0e9eef37a0b087eda4774ed07923aa31201feab8
Alexandre <alexandre.beeckmans@student.howest.be>
2024-02-21 22:15:45 +0100

    Create ImageComponent

---
+++ Minigin/ImageComponent.cpp
@@ -0,0 +1,26 @@
+#include "ImageComponent.h"
+#include "ResourceManager.h"
+#include "Renderer.h"
+#include "GameObject.h"
+#include "Texture2D.h"
+
+#include "iostream"
+
+dae::ImageComponent::ImageComponent(GameObject* pGameObject, const std::string& filePath, const float relativeX, const float relativeY) :
+	BaseComponent::BaseComponent(pGameObject)
+{
+	m_RelativePosition.SetPosition(relativeX, relativeY, 0);
+	m_pTexture = ResourceManager::GetInstance().LoadTexture(filePath);
+}
+
+void dae::ImageComponent::Update(const float deltaTime)
+{
+	if(false)
+		std::cout << deltaTime << std::endl;
+}
+
+void dae::ImageComponent::Render() const
+{
+	const auto& pos = m_pParent->GetPosition();
+	Renderer::GetInstance().RenderTexture(*m_pTexture.get(), pos.x + m_RelativePosition.GetPosition().x, pos.y + m_RelativePosition.GetPosition().y);
+}
---
+++ Minigin/ImageComponent.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "BaseComponent.h"
+#include "Transform.h"
+
+#include <memory>
+#include <string>
+
+namespace dae
+{
+	class Texture2D;
+
+	class ImageComponent final : public BaseComponent
+	{
+	public:
+		ImageComponent(GameObject* pGameObject, const std::string& filePath, const float relativeX, const float relativeY);
+		~ImageComponent() { m_pTexture = nullptr; };
+
+		ImageComponent(const ImageComponent& other) = default;
+		ImageComponent(ImageComponent&& other) = default;
+		ImageComponent& operator=(const ImageComponent& other) = default;
+		ImageComponent& operator=(ImageComponent&& other) = default;
+
+		void virtual Update(const float deltaTime) override;
+		void virtual Render() const override;
+
+	private:
+		std::shared_ptr<Texture2D> m_pTexture{};
+		Transform m_RelativePosition{};
+	};
+}
+
+
