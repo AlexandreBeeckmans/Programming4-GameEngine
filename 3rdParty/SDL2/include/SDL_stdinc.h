44321549796fec753d3ff096e9ff5ae2f51eb305
Alexandre <alexandre.beeckmans@student.howest.be>
2024-02-21 20:46:25 +0100

    Silence 3 of the 4 fallthrough warnings in  SDL_stdinc.h

--- 3rdParty/SDL2/include/SDL_stdinc.h
+++ 3rdParty/SDL2/include/SDL_stdinc.h
@@ -541,11 +541,14 @@ SDL_FORCE_INLINE void SDL_memset4(void *dst, Uint32 val, size_t dwords)
         return;
     }
     switch (dwords % 4) {
-        case 0: do {    *_p++ = _val;   SDL_FALLTHROUGH;
-        case 3:         *_p++ = _val;   SDL_FALLTHROUGH;
-        case 2:         *_p++ = _val;   SDL_FALLTHROUGH;
-        case 1:         *_p++ = _val;   SDL_FALLTHROUGH;
-        } while ( --_n );
+        case 0: 
+            do 
+            {    
+                *_p++ = _val;   SDL_FALLTHROUGH;    [[fallthrough]];
+                case 3:         *_p++ = _val;   SDL_FALLTHROUGH;    [[fallthrough]];
+                case 2:         *_p++ = _val;   SDL_FALLTHROUGH;    [[fallthrough]];
+                case 1:         *_p++ = _val;
+            } while (--_n);
     }
 #endif
 }
