3709c5b162941078a789e89de0434414e9b49bf9
Alexandre <alexandre.beeckmans@student.howest.be>
2024-03-05 14:09:02 +0100

    Correct rotating component
    - avoid complicated use of center inside of the class
    
    - reduce angle value when over 2pi

--- Minigin/RotatingComponent.cpp
+++ Minigin/RotatingComponent.cpp
@@ -3,33 +3,24 @@
 #include "GameObject.h"
 #include "Time.h"
 
+#include <numbers>
+
+
 dae::RotatingComponent::RotatingComponent(GameObject* pGameObject, const float speed, const float radius) :
 	BaseComponent(pGameObject),
 	m_Speed{speed},
 	m_Radius{radius}
 {
-	//The center of rotation is the current position of the player when init
-	m_Center.x = GetOwner()->GetWorldPosition().x;
-	m_Center.y = GetOwner()->GetWorldPosition().y;
 }
 
 void dae::RotatingComponent::Update()
 {
 	m_Angle += m_Speed * Time::GetInstance().GetDeltaTime();
+	if (m_Angle >= 2 * std::numbers::pi_v<decltype(m_Angle)>)
+		m_Angle -= 2 * std::numbers::pi_v<decltype(m_Angle)>;
+
 	float newX{ m_Radius * cosf(m_Angle)};
 	float newY{ m_Radius * sinf(m_Angle) };
-
-
-	//If object has no parent, will turn around its world position
-	if (!GetOwner()->GetParent())
-	{
-		newX += m_Center.x;
-		newY += m_Center.y;
-	}
-
-	//So if the player has a parent, it will turn around its parent which is its 0;0 local position
 		
-
-
 	GetOwner()->SetLocalPosition(newX, newY);
 }
--- Minigin/RotatingComponent.h
+++ Minigin/RotatingComponent.h
@@ -23,7 +23,7 @@ namespace dae
 
 		const float m_Speed{ 5.0f };
 		float m_Angle{ 0.0f };
-		glm::vec2 m_Center{ 0,0 };
+		//glm::vec2 m_Center{ 0,0 };
 		float m_Radius{ 15.0f };
 
 	};
